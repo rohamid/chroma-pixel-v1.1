7d8bf793548bc6d9138dcb5558eebb0191085cbc
rohamid <rohamid90@gmail.com>
2024-07-26 20:26:32 +0700

    Initial commit

---
+++ Core/Inc/sdram.h
@@ -0,0 +1,24 @@
+/*
+ * sdram.h
+ *
+ *  Created on: Jul 26, 2024
+ *      Author: rd-dev
+ */
+
+#ifndef INC_SDRAM_H_
+#define INC_SDRAM_H_
+
+#include "main.h"
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+bool sdramInit(void);
+
+#ifdef __cplusplus
+ }
+#endif
+
+
+#endif /* INC_SDRAM_H_ */
